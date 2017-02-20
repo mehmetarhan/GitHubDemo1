934af34b4f2a49bbe36c6f8fc4272afa80037edb
Mehmet Tarhan <mhmtrhn@Mehmets-MacBook-Air.local>
2017-02-20 21:31:49 +0300

    firstCommit

---
+++ GitHubDemo1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubDemo1
+//
+//  Created by Mehmet Tarhan on 2/20/17.
+//  Copyright © 2017 Mehmet Tarhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Let's see if GitHub is working")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
