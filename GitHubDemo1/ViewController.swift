557a6c5c868eef5eedcbe40f6e51110fa478b4a4
Mehmet Tarhan <mhmtrhn@Mehmets-MacBook-Air.local>
2017-02-20 21:37:22 +0300

    firstCommit

--- GitHubDemo1/ViewController.swift
+++ GitHubDemo1/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Let's see if GitHub is working")
+        print("Is it working?")
     }
 
     override func didReceiveMemoryWarning() {
